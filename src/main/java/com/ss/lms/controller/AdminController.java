4cdaba030b0958bd780d1877347dcd57f6571bb1
Abdi <abdirahman.noor@smoothstack.com>
2019-10-11 15:54:35 -0400

    admin controller

--- src/main/java/com/ss/lms/controller/AdminController.java
+++ src/main/java/com/ss/lms/controller/AdminController.java
@@ -1,4 +1,15 @@
 package com.ss.lms.controller;
 
+import com.ss.lms.services.AdminServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lms/admin/")
 public class AdminController {
+    @Autowired
+    private AdminServices adminServices;
+
+
 }
